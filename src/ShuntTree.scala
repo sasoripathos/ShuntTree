0a44799c2e2abee5ecc4c8ed8cd314ec03a5a7c6
sasoripathos <sasori.pan.jerry@gmail.com>
2024-12-05 13:53:42 +0100

    Fix syntax problem in definition of ShuntTree

--- src/ShuntTree.scala
+++ src/ShuntTree.scala
@@ -7,18 +7,18 @@ object ShuntTreeObject {
   // ST a b
   sealed abstract class ShuntTree[A, B]
   // T a
-  case class T[A](value: A) extends ShuntTree[A, B]
+  case class T[A, B](value: A) extends ShuntTree[A, B]
   // N (ST a b) (ST. a b) (ST a b)
   case class N[A, B](left: ShuntTree[A, B], middle: ShuntContext[A, B], right: ShuntTree[A, B]) extends ShuntTree[A, B]  
 
   // (ST. a b)
   sealed abstract class ShuntContext[A, B]
   // H. b
-  case class H[B](value: B) extends ShuntContext[A, B]
+  case class H[A, B](value: B) extends ShuntContext[A, B]
   // L. (ST. a b) (ST. a b) (ST a b)
-  case class L[B](left: ShuntContext[A, B], middle: ShuntContext[A, B], right:  ShuntTree[A, B]) extends ShuntContext[A, B]
+  case class L[A, B](left: ShuntContext[A, B], middle: ShuntContext[A, B], right:  ShuntTree[A, B]) extends ShuntContext[A, B]
   // R. (ST a b) (ST. a b) (ST. a b)
-  case class R[B](left: ShuntTree[A, B], middle: ShuntContext[A, B], right:  ShuntContext[A, B]) extends ShuntContext[A, B]
+  case class R[A, B](left: ShuntTree[A, B], middle: ShuntContext[A, B], right:  ShuntContext[A, B]) extends ShuntContext[A, B]
   
 
 }
