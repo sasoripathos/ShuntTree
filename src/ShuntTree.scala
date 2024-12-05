7ff82bc7064f5c692432afc69b0bce73e959fbc9
sasoripathos <sasori.pan.jerry@gmail.com>
2024-12-05 13:46:32 +0100

    Add basic shunt tree definition, wait for test

--- src/ShuntTree.scala
+++ src/ShuntTree.scala
@@ -2,5 +2,23 @@ import stainless.lang.*
 import stainless.collection.*
 import stainless.annotation.*
 
-object ShuntTree {
+object ShuntTreeObject {
+
+  // ST a b
+  sealed abstract class ShuntTree[A, B]
+  // T a
+  case class T[A](value: A) extends ShuntTree[A, B]
+  // N (ST a b) (ST. a b) (ST a b)
+  case class N[A, B](left: ShuntTree[A, B], middle: ShuntContext[A, B], right: ShuntTree[A, B]) extends ShuntTree[A, B]  
+
+  // (ST. a b)
+  sealed abstract class ShuntContext[A, B]
+  // H. b
+  case class H[B](value: B) extends ShuntContext[A, B]
+  // L. (ST. a b) (ST. a b) (ST a b)
+  case class L[B](left: ShuntContext[A, B], middle: ShuntContext[A, B], right:  ShuntTree[A, B]) extends ShuntContext[A, B]
+  // R. (ST a b) (ST. a b) (ST. a b)
+  case class R[B](left: ShuntTree[A, B], middle: ShuntContext[A, B], right:  ShuntContext[A, B]) extends ShuntContext[A, B]
+  
+
 }
