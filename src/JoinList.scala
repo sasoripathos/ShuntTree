39f0b8cec9972b0f7af30ed5ae62befa7ad64012
sasoripathos <sasori.pan.jerry@gmail.com>
2024-12-17 19:35:52 +0100

    Clean unused code at this point

--- src/JoinList.scala
+++ src/JoinList.scala
@@ -6,11 +6,6 @@ object JoinListObject {
 
   // helper funtions
   def max(x: BigInt, y: BigInt) = if x >= y then x else y
-  // def pow(base: BigInt, exp: BigInt): BigInt = {
-  //   require(exp >= 0) // only handle non neg exp
-  //   if (exp == BigInt(0)) then BigInt(1)
-  //   else base * pow(base, exp - BigInt(1))
-  // }
   def abs(x: BigInt) = if BigInt(0) > x then -x else x
 
   // JoinList[T]
@@ -23,7 +18,6 @@ object JoinListObject {
   case class Join[T](left: JoinList[T], right: JoinList[T]) extends JoinList[T] {
     require(
       left != Empty[T]() && right != Empty[T]() // as define in the paper, left and right cannot be empty
-      //&& BigInt(-1) <= left.height - right.height && left.height - right.height <= BigInt(1) // ensure the 2 lists are balanced
     )
   }
 
@@ -44,69 +38,10 @@ object JoinListObject {
         case _ => true // true for empty and single node
       }
     }
-
-    // def leftRotate(other: JoinList[T]): JoinList[T] = {
-    //   // left rotate the tree should not change the meaning, here assume jl is left subtree and other is right subtree and have different height
-    //   require(jl.height < other.height && !jl.isEmpty && !other.isEmpty)
-    //   heightForNonEmpty(jl) // jl height >= 1
-    //   assert(other.height >= 2)
-    //   other match {
-    //     case Join(rl, rr) =>  {
-    //       // (l ++ rl) ++ rr == l ++ (rl ++ rr)
-    //       ListSpecs.appendAssoc(jl.toList, rl.toList, rr.toList)
-    //       Join(Join(jl, rl), rr)
-    //     }
-    //   }
-    // }.ensuring(_.toList == jl.toList ++ other.toList)
-
-    // def rightRotate(other: JoinList[T]): JoinList[T] = {
-    //   // right rotate the tree should not change the meaning, here assume jl is left subtree and other is right subtree and have different height
-    //   require(jl.height > other.height && !jl.isEmpty && !other.isEmpty)
-    //   heightForNonEmpty(other) // jl height >= 1
-    //   assert(jl.height >= 2)
-    //   jl match {
-    //     case Join(ll, lr) =>  {
-    //       // (ll ++ lr) ++ other == li ++ (lr ++ other)
-    //       ListSpecs.appendAssoc(ll.toList, lr.toList, other.toList)
-    //       Join(ll, Join(lr, other))
-    //     }
-    //   }
-    // }.ensuring(_.toList == jl.toList ++ other.toList)
-
-    // def rebalance: JoinList[T] = {
-    //   jl match {
-    //     case Join(l, r) => {
-    //       if (jl.isBalanced) then jl
-    //       else {
-            
-            
-    //       }
-    //     }
-    //     case _ => {
-    //       assert(jl.isBalanced)
-    //       jl
-    //     }
-    //   }
-    // }.ensuring(_.toList == jl.toList && _.isBalanced)
   }
 
   // Proof for tree properties
-  // P1. a JoinList is always balanced
-  // def joinListIsAlwaysBalanced[T](jl: JoinList[T]): Unit = {
-  //   jl match {
-  //     case Join(l, r) => {
-  //       assert(BigInt(-1) <= l.height - r.height && l.height - r.height <= BigInt(1))
-  //       joinListIsAlwaysBalanced(l)
-  //       joinListIsAlwaysBalanced(r)
-  //     } // both child are balanced, constructor ensured the hight differences
-  //     case _ => {
-  //       assert(jl.isBalanced)
-  //       ()
-  //     } // true for empty and single node
-  //   }
-  // }.ensuring(jl.isBalanced)
-
-  // p2: a JoinList with element has at least size 1
+  // p1: a JoinList with element has at least size 1
   def sizeForNonEmpty[T](jl: JoinList[T]): Unit = {
     require(!jl.isEmpty)
     jl match {
@@ -122,36 +57,6 @@ object JoinListObject {
       }
     }
   }.ensuring(jl.size >= BigInt(1))
-
-  // // P3: a JoinList with element has at least hight 1
-  // def heightForNonEmpty[T](jl: JoinList[T]): Unit = {
-  //   require(!jl.isEmpty)
-  //   jl match {
-  //     case Single(x) => {
-  //       assert(jl.height == BigInt(1))
-  //       ()
-  //     }
-  //     case Join(l, r) => {
-  //       heightForNonEmpty(l)
-  //       heightForNonEmpty(r)
-  //       assert(jl.height >= BigInt(1))
-  //     }
-  //   }
-  // }.ensuring(jl.height >= BigInt(1))
-
-  // // P4: a JoinList with at least 2 element has at least hight 2
-  // def heightForAtLeastTwoElement[T](jl: JoinList[T]): Unit = {
-  //   require(jl.size >= BigInt(2))
-  //   assert(jl.isJoin)
-  //   jl match {
-  //     case Join(l, r) => {
-  //       heightForNonEmpty(l)
-  //       heightForNonEmpty(r)
-  //       assert(jl.height >= BigInt(1) + BigInt(1))
-  //       ()
-  //     }
-  //   }
-  // }.ensuring(jl.height >= BigInt(2))
 
 
 
@@ -218,14 +123,6 @@ object JoinListObject {
       }
     }
 
-    def isJoin: Boolean = {
-      // Check if a JoinList is of shape Join(l, r)
-      jl match {
-        case Join(l, r) => true
-        case _ => false
-      }
-    }
-
     def head: T = {
       // Return the first element of the JoinList, only work on non-empty list
       require(!jl.isEmpty)
@@ -253,22 +150,6 @@ object JoinListObject {
   // foldl, foldr?
   // ...... maybe more in.
   extension[T](jl: JoinList[T]) {
-    // def <>(other: JoinList[T]): JoinList[T] = {
-    //   if (jl.isEmpty) then {
-    //     assert(other.height >= 0)
-    //     other
-    //   }
-    //   else if (other.isEmpty) then jl
-    //   else {
-    //     val ans = Join(jl, other)
-    //     assert(ans.height == max(jl.height, other.height) + BigInt(1))
-    //     ans
-    //   }
-    // }.ensuring(
-    //   res => ((res.toList == jl.toList ++ other.toList)
-    //   && res.height <= max(jl.height, other.height) + BigInt(1)
-    //   && res.height >= max(jl.height, other.height))
-    // )
 
     def ++(other: JoinList[T]): JoinList[T] = {
       require(jl.isBalanced && other.isBalanced)
@@ -277,8 +158,6 @@ object JoinListObject {
       else if (other.isEmpty) then jl // if other is empty, just return self
       else {
         // if neither are empty
-        // heightForNonEmpty(jl)
-        // heightForNonEmpty(other)
         if (BigInt(-1) <= jl.height - other.height && jl.height - other.height <= BigInt(1)) {
           // 1. if 2 trees have good heights, just join
           Join(jl, other)
@@ -290,26 +169,18 @@ object JoinListObject {
               if (rr.height >= rl.height) {
                 // since newl.height at most increase 1, would always be balanced
                 Join(jl ++ rl, rr)
-                // (jl ++ rl) <> rr
               } else {
                 // otherwise,
                 assert(other.height == rl.height + 1)
                 rl match {
                   case Join(rll, rlr) => {
                     ListSpecs.appendAssoc(jl.toList, rll.toList, rlr.toList)
-                    // assert(rl.height == max(rll.height, rlr.height) + 1) // by definition
-                    // assert(rll.isBalanced && rlr.isBalanced && rl.isBalanced)
-                    // balancedHeightProp(rll: JoinList[T], rlr: JoinList[T])
-                    // assert((rll.height == rl.height - BigInt(2)) ==> (rlr.height == rl.height - BigInt(1))) 
-                    // assert((rlr.height == other.height - 3) ==> (rll.height == other.height - 2))
                     val newl = jl ++ rll
                     ListSpecs.appendAssoc(newl.toList, rlr.toList, rr.toList)
                     if (newl.height == other.height - 3) {
                       Join(Join(newl, rlr), rr)
-                      // (newl <> rlr) <> rr
                     } else {
                       Join(newl, Join(rlr, rr))
-                      // newl <> (rlr <> rr)
                     }
                   }
                 }
@@ -324,7 +195,6 @@ object JoinListObject {
               if (ll.height >= lr.height) {
                 // Since lr ++ other would at most increase height by 1
                 Join(ll, lr ++ other)
-                // ll <> (lr ++ other)
               } else {
                 lr match {
                   case Join(lrl, lrr) => {
@@ -333,10 +203,8 @@ object JoinListObject {
                     ListSpecs.appendAssoc(ll.toList, lrl.toList, newr.toList)
                     if (newr.height == jl.height - 3) {
                       Join(ll, Join(lrl, newr))
-                      // ll <> (lrl <> newr)
                     } else {
                       Join(Join(ll, lrl), newr)
-                      // (ll <> lrl) <> newr
                     }
                   }
                 }
