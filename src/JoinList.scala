d33a50eebe3f2193c41b6345361f011ca170804b
sasoripathos <sasori.pan.jerry@gmail.com>
2024-12-18 11:50:51 +0100

    Draft for prepend

--- src/JoinList.scala
+++ src/JoinList.scala
@@ -114,7 +114,19 @@ object JoinListObject {
       }
     }.ensuring(_.toList == jl.toList.tail)
 
-    // naive concat, unbalanced version
+    def ::(t: T): JoinList[T] = {
+      // Prepend an element to JoinList
+      jl match {
+        case Empty() => Single(t)
+        case Single(x) = Join(Single(t), jl)
+        case Join(l, r) => {
+          val newl = t :: l
+          assert(newl.toList == Cons(t, l.toList))
+          ListSpecs.reverseAppend(newl.toList, r.toList)
+          newl ++ r
+        }
+      }
+    }.ensuring(_.toList = t :: jl.toList)
     
   }
   
