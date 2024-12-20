80d37d3c4883f29a4a81a9270197623afc5c95a5
sasoripathos <sasori.pan.jerry@gmail.com>
2024-12-20 17:32:59 +0100

    Add some properties for stainless list

--- src/Helper.scala
+++ src/Helper.scala
@@ -47,7 +47,20 @@ object Helper {
     }
   }.ensuring((l1 ++ l2).foldLeft(basecase)(f) == l2.foldLeft(l1.foldLeft(basecase)(f))(f))
 
-  // def prependEQ
+  def prependEqualListContact[T](l: List[T], x: T): Boolean = {
+    l match {
+      case Nil() => {
+        assert(x :: l == Cons(x, Nil[T]()))
+        true
+      }
+      case Cons(y, ys) => {
+        assert(x :: l == Cons(x, l)) // by definition
+        assert(Cons(x, Nil[T]()) ++ l == Cons(x, l))
+        true
+      }
+    }
+    x :: l == Cons(x, Nil[T]()) ++ l
+  }.holds
 
   def distributiveOfMap[T, R](l1: List[T], l2: List[T], f: T => R): Unit = {
     if (l1.isEmpty) then {
@@ -86,6 +99,24 @@ object Helper {
   //   }
   // }.ensuring(l1 ++ l2 == l1.head :: (l1.tail ++ l2))
 
+  def foldLeftTailProp[T, R](l: List[T], t: T, f: (R, T) => R, zero: R): Boolean = {
+    val em = Nil[T]()
+    l match {
+      case Nil() => {
+        assert(l :+ t == Cons(t, em)) 
+        assert((l :+ t).foldLeft(zero)(f) == em.foldLeft(f(zero, t))(f))
+        true
+      }
+      case Cons(x, xs) => {
+        assert(l :+ t == Cons(x, xs :+ t))
+        assert((l :+ t).foldLeft(zero)(f) == (xs :+ t).foldLeft(f(zero, x))(f))
+        assert(xs.foldLeft(f(zero, x))(f) == l.foldLeft(zero)(f)) // by definition
+        foldLeftTailProp(xs, t, f, f(zero, x)) // == f(xs.foldLeft(f(zero, x))(f), t) == f(l.foldLeft(zero)(f), t)
+      }
+    }
+    (l :+ t).foldLeft(zero)(f) == f(l.foldLeft(zero)(f), t)
+  }.holds
+
   def listAggregation[T, R](l: List[T], combine: (R, R) => R, convert: T => R): R = {
     // Precondition 1: the combine is associative
     require(forall((x: R, y: R, z: R) => combine(combine(x, y), z) == combine(x, combine(y, z))))
@@ -114,7 +145,7 @@ object Helper {
   //         // val lagg = Cons(x, l2)
   //         assert(l1 ++ l2 == Cons(x, l2)) // by definition
   //         // val covertedL2 = l2.map(convert)
-  //         assert(listAggregation(l1 ++ l2, combine, convert) == l2.map(convert).foldLeft(convert(x))(combine)) // LHS is this
+  //         assert(listAggregation(l1 ++ l2, combine, convert) == l2.map(convert).foldLeft(convert(x))(combine)) // LHS as proved above
   //         assert(listAggregation(l1, combine, convert) == convert(x))
   //         l2 match {
   //           case Cons(y, Nil()) => {
@@ -127,10 +158,8 @@ object Helper {
   //             assert(listAggregation(l2, combine, convert) == ys.map(convert).foldLeft(convert(y))(combine)) // provide above
   //             assert(l2.map(convert) == Cons(convert(y), ys.map(convert))) // by definition
   //             assert(l2.map(convert).foldLeft(convert(x))(combine) == ys.map(convert).foldLeft(combine(convert(x), convert(y)))(combine))
-  //             // assert(l2 == Cons(y, Nil()) ++ ys)
-  //             assert(l2 == y :: ys)
-  //             assert(Cons(y, Nil()) == y :: Nil())
-  //             assert(ListSpecs.reverseAppend(Cons(y, Nil()), ys) && ListSpecs.reverseReverse(Cons(y, Nil()) ++ ys))
+  //             assert(l2 == y :: ys) // by definition
+  //             prependEqualListContact[T](ys, y)
   //             assert(l2 == Cons(y, Nil()) ++ ys)
   //             listAggregationDistributive(Cons(y, Nil()), ys, combine, convert) // if this holde , RHS = combine(convert(x), combine(convert(y), agg(ys)))
   //             // by association, RHS = combine(combine(convert(x), convert(y)), ys) = ys.foldleft(combine(x, y))(combine)
@@ -140,6 +169,21 @@ object Helper {
   //       }
   //       case Cons(x, xs) => {
   //         assert(!xs.isEmpty)
+  //         assert(l1 ++ l2 == Cons(x, xs ++ l2)) // by definition
+  //         assert(listAggregation(l1 ++ l2, combine, convert) == (xs ++ l2).map(convert).foldLeft(convert(x))(combine)) // LHS as proved above
+  //         assert(listAggregation(l1, combine, convert) == xs.map(convert).foldLeft(convert(x))(combine)) // as proved above
+  //         l2 match {
+  //           case Cons(y, Nil()) => {
+  //             assert(listAggregation(l2, combine, convert) == convert(y))
+  //             ListSpecs.snocIsAppend(xs, y)
+  //             assert(xs ++ l2 == xs :+ y)
+  //             assert((xs ++ l2).map(convert) == (xs :+ y).map(convert))
+  //             // TODO
+  //             assert(foldLeftTailProp(xs, y, combine, zero: R))
+  //             assert(listAggregation(l1 ++ l2, combine, convert) == combine(xs.map(convert).foldLeft(convert(x))(combine), convert(y)))
+  //             true
+  //           }
+  //         }
   //         true
   //       }
   //     }
